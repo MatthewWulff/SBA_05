b269c4b9483d00b4e6976dfcb8cd0650e3ad030b
Matthew <caspr117@gmail.com>
2025-06-25 19:21:59 -0400

    connect to mongoose

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ import "dotenv/config"
 const app = express()
 
 const PORT = process.env.PORT
+app.use(express.json())
+const db = await mongoose.connect(process.env.MONGO_URL)
+
+
 
 app.listen(PORT,()=> console.log("port listening"))
