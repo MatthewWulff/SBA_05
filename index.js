d7beeb1f27453d786d9c273b0763b8a992c0c672
Matthew <caspr117@gmail.com>
2025-06-25 19:16:22 -0400

    server

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 import express from "express"
 import mongoose from "mongoose"
 import "dotenv/config"
+
+const app = express()
+
+const PORT = process.env.PORT
+
+app.listen(PORT,()=> console.log("port listening"))
