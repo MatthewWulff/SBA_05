9d463a3270b7356830580798e9523c81a4203b71
Matthew <caspr117@gmail.com>
2025-06-25 19:28:23 -0400

    schema

--- model/Accounts.js
+++ model/Accounts.js
@@ -1,1 +1,22 @@
 import mongoose from "mongoose"
+
+const accountSchema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+    unique: true,
+    minLength: 10,
+    maxLength: 10,
+  },
+  accountbalance: {
+    type: Number,
+    default: 0,
+  },
+})
+
+export default mongoose.model("Account", accountSchema)
