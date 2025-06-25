02253b62e68b353dd762757026d47efc7606c69a
Matthew <caspr117@gmail.com>
2025-06-25 19:35:53 -0400

    get all but account balance

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import express from "express"
 import mongoose from "mongoose"
 import "dotenv/config"
-import Account from "./models/Account.js"
+import Account from "./model/Accounts.js"
 
 const app = express()
 
 const PORT = process.env.PORT
 app.use(express.json())
 const db = await mongoose.connect(process.env.MONGO_URL)
+
+
+app.get("/accounts" , async(req,res)=>{
+    const accounts = await Account.find({}, {accountbalance:0})
+    res.json(accounts)
+})
 
 
 
